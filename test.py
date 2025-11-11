d4562e79f08d9affe2918f7c538252db0e437328
amirx011 <amirmz100101@gmail.com>
2025-11-11 12:15:45 +0330

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+print('this is a test')
+
+
+test_list = [0,0,1,2,3,5,8,13,21,34,55,89]
+print(sum(test_list))
+print(min(test_list))
+
+sum_test_list = 231
+if sum_test_list == 12:
+    print('jj')
+elif sum_test_list == 231 :
+    print('hh')
+    
+
+
